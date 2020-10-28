aab526560ba5e6b6879d9fbb909901a706c39c09
firstofficerfurkan <firstofficerfurkan@gmail.com>
2020-10-28 10:47:33 +0300

    asdf

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(){
 printf("furkan's print statement");
 printf("emir kaan's print statement")
 printf("Tuna's print statement");
-
 printf("furkan's second statement");
+printf("furkans third statement");
 return 0;
 }
