0cc8c378d5a1c8886d14ce6be6002fb4b5187dc2
firstofficerfurkan <41448685+firstofficerfurkan@users.noreply.github.com>
2020-10-28 22:21:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 int main(){
 
-printf("furkan's print statement");
-printf("emir kaan's print statement")
-printf("Tuna's print statement");
-printf("furkan's second statement");
-printf("furkans third statement");
-printf("furkans 4 statement");
-printf("furkans 5 statement");
 return 0;
 }
