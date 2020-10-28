ad3759c79fc34c2dab2c2c7ee94f04c07aca234b
firstofficerfurkan <41448685+firstofficerfurkan@users.noreply.github.com>
2020-10-28 10:48:28 +0300

    Update main.c
    online

--- main.c
+++ main.c
@@ -6,5 +6,7 @@ printf("emir kaan's print statement")
 printf("Tuna's print statement");
 printf("furkan's second statement");
 printf("furkans third statement");
+printf("furkans 4 statement");
+printf("furkans 5 statement");
 return 0;
 }
