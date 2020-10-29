90841d49b417cd0548f81e285ae301a7d619a8c3
firstofficerfurkan <firstofficerfurkan@gmail.com>
2020-10-29 17:30:15 +0300

    new ahmet statemtn inside furkan func

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def ahmetFunction():
   print("Hello from a function")
 def furkanFunction():
-  print("Hello from a function")
-
-
+	print("Hello from a function")
+	print("this is ahmet statement inside furkan func")
+	print("this is second ahmet statement inside furkan func")
 print("furkanfu rkan furkan")
 
 
