787a93cbe70393851fcd4415afb3472fa6347ebe
firstofficerfurkan <firstofficerfurkan@gmail.com>
2020-10-29 17:28:08 +0300

    furkan add to ahmet func

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def ahmetFunction():
-  print("Hello from a function")
+	print("Hello from a function")
+	print("this is a furkan statement inside ahmet function")
+	print("this is the second furkan statement inside ahmet function")
 def furkanFunction():
   print("Hello from a function")
 
